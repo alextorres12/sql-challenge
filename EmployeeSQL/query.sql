18c9900b77ed0773cb7d5ffbc71352e9eb144da0
Alex Torres <62618236+alextorres12@users.noreply.github.com>
2020-11-03 14:59:31 -0600

    Adds final solutions and ERD image

--- EmployeeSQL/query.sql
+++ EmployeeSQL/query.sql
@@ -74,7 +74,9 @@ WHERE dept_name = 'Sales' OR
 dept_name = 'Development';
 
 --8) In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
-SELECT COUNT()
-
+SELECT last_name, COUNT(last_name) AS "Employee Count"
+FROM employees
+GROUP BY last_name
+ORDER BY "Employee Count" DESC;
 
 
